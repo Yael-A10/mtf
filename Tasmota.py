d055e408d0c56ba6241dc8dd82e3caea9f83120d
Coriolan Weihrauch <coriolan@miastudio.in>
2022-01-18 15:06:45 +0530

    v2

--- Tasmota.py
+++ Tasmota.py
@@ -9,45 +9,50 @@ def download(file):
     url = 'http://ota.tasmota.com/tasmota/release/'+file
     r = requests.get(url, allow_redirects=True)
     open(path + file, 'wb').write(r.content)
+
 def flash(file):
     esptool.main(['erase_flash'])
     esptool.main(['write_flash', '-fs', '1MB', '-fm', 'dout', '0x0', path + file])
 
-impFlash = False
-while impFlash != 'y' and impFlash != 'n':
-    impFlash = input('Do you want to flash [y/n]: ').lower().strip()
-if impFlash == 'y':
-    available_versions = []
-    for x in versions:
-        if os.path.exists(path + x):
-            available_versions.append(x)
-    print(available_versions)
-    if available_versions:
-        while True:
-            impUseFile = input('Do you want to use existing file(s); '+'; '.join(available_versions)+' [y/n]: ')
-            if impUseFile == 'y' or impUseFile == 'n':
-                break
-        if impUseFile == 'y':
-            if len(available_versions) > 1:
-                while True:
-                    for x in available_versions:
-                        print(x + ' [' + str(available_versions.index(x)) + ']')
-                    impWhichFile = input('Which file would you like to use: ')
-                    if impWhichFile.isnumeric() and int(impWhichFile) >= 0 and int(impWhichFile) < len(available_versions):
-                        break
-                download(available_versions[int(impWhichFile)])
-                flash(available_versions[int(impWhichFile)])
-            else:
-                download(available_versions[0])
-                flash(available_versions[0])
+def question(q):
+    data = ''
+    while data != 'y' and data !='n':
+        data = input(q + ' [y/n]: ').lower().strip()
+    return data
+
+def findAvailable():
+  availableVersions = []
+  for x in versions:
+    if os.path.exists(path + x):
+      availableVersions.append(x)
+  return availableVersions
+
+def findFile():
+    available = findAvailable()
+    if available and question('Do you want to use existing file(s); '+'; '.join(available)) == 'y':
+        if len(available) > 1:
+            availableIndex = []
+            for x in available:
+                print(x + ' [' + str(available.index(x)) + ']')
+                availableIndex.append(available.index(x))
+            data = '-1'
+            while not data.isnumeric() or int(data) in availableIndex:
+                data = input('Which file would you like to use: ')
+            return available[int(data)]
+        else:
+            return available[0]
     else:
-        while True:
-            for x in versions:
-                print(x + ' [' + str(versions.index(x)) + ']')
-            impWhichVersion = input('Which version would you like to use: ')
-            if impWhichVersion.isnumeric() and int(impWhichVersion) >= 0 and int(impWhichVersion) < len(versions):
-                break
-        download(versions[int(impWhichVersion)])
-        flash(versions[int(impWhichVersion)])
+        versionIndex = []
+        for x in versions:
+            print(x + ' [' + str(versions.index(x)) + ']')
+            versionIndex.append(versions.index(x))
+        data = '-1'
+        while not data.isnumeric() or int(data) not in versionIndex:
+            data = input('Which version would you like to use: ')
+        download(versions[int(data)])
+        return versions[int(data)]
+
+if question('Do you want to flash') == 'y':
+    flash(findFile())
 else:
     print('i dont want to flash')
